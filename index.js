0e5b88cc934fb06614f626d39335c75e1aed552a
EvgeniiAtYourService <eugeniotorneros@gmail.com>
2021-03-12 16:52:17 +0500

    Refactored mockup

---
+++ index.js
@@ -0,0 +1,9 @@
+// $('navbar').on('click', function(){
+//   // $('.navbar-collapse').css('style', "red");
+//   alert('hi');
+// });
+$('.navbar-brand, .dropdown-item, .remove-collapse').click(function(){
+  $('#navbarSupportedContent').collapse('hide');
+});
+
+ 
