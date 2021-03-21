45e49c4acc09e478b3c8f8e614bc28aacba2a3d7
EvgeniiAtYourService <eugeniotorneros@gmail.com>
2021-03-21 16:14:27 +0500

    Modify parallax effect so the background image and other components have different scrolling speed

--- index.js
+++ index.js
@@ -6,5 +6,12 @@ $('.navbar-brand, .dropdown-item, .remove-collapse').click(function(){
   $('#navbarSupportedContent').collapse('hide');
 });
 
-
- 
+/*2nd try parallax CSS+JS*/
+document.querySelector("body").onscroll = function myFunction() {
+   var scrolltotop = document.scrollingElement.scrollTop;
+   var target = document.querySelector("body");
+   var xvalue = "center";
+   var factor = 0.5;
+   var yvalue = scrolltotop * factor;
+   target.style.backgroundPosition = xvalue + " " + yvalue + "px";
+ }
