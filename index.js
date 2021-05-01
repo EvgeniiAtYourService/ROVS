c7428d79f0ba3bef67e7c80bc52fbdd5c509a176
EvgeniiAtYourService <eugeniotorneros@gmail.com>
2021-05-01 15:13:20 +0500

    Delete comments & disabled elements & classes

--- index.js
+++ index.js
@@ -1,12 +1,8 @@
-// $('navbar').on('click', function(){
-//   // $('.navbar-collapse').css('style', "red");
-//   alert('hi');
-// });
 $('.navbar-brand, .dropdown-item, .remove-collapse').click(function(){
   $('#navbarSupportedContent').collapse('hide');
 });
 
-/*2nd try parallax CSS+JS*/
+/* parallax CSS+JS */
 document.querySelector("body").onscroll = function myFunction() {
    var scrolltotop = document.scrollingElement.scrollTop;
    var target = document.querySelector("body");
@@ -18,11 +14,11 @@ document.querySelector("body").onscroll = function myFunction() {
 
    //Back to Top button
    if ($(this).scrollTop() > 500) {
-   				// document.querySelector(".back-to-top").style.display = "inline";       version with block -> inline display
+   				
           document.querySelector(".back-to-top").classList.add("active");
    			} else {
    				document.querySelector(".back-to-top").classList.remove("active");
    			}
 
  }
- // document.querySelector(".back-to-top").style.display = "inline"; // Other way to do appearing of the button
+ 
